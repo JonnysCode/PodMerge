cbaa62742464995c5476d09a632bd8b6da733516
Jonathan Gruss <jonathangruss@hotmail.com>
2023-06-19 14:11:06 +0200

    new yjs json-ld

--- apps/local-first-extension/src/y/YjsJsonLD.js
+++ apps/local-first-extension/src/y/YjsJsonLD.js
@@ -1,3 +1,4 @@
+import { SyncedMap } from '@syncedstore/core';
 import { JsonLD } from '../LD/JsonLd';
 
 class YjsJsonLD extends JsonLD {
@@ -15,5 +16,27 @@ class YjsJsonLD extends JsonLD {
 
   static fromYStore(store, url = null) {
     return super.fromYStore(store, url);
+  }
+
+  expand(target, prop = null) {
+    if (!prop && typeof target !== 'object') {
+      throw new Error('Cannot expand a primitive without a propertyName');
+    }
+
+    if (typeof target[prop] === 'object') {
+      target = target[prop];
+    }
+
+    let value = prop ? target[prop] : { ...target };
+    if (!value.isExpanded()) {
+      // TODO: Make it a SyncedMap
+      if (prop) {
+        target[prop] = new SyncedMap();
+        target[prop]['@value'] = value;
+      } else {
+        Object.keys(target).forEach((key) => delete target[key]);
+        target['@value'] = value;
+      }
+    }
   }
 }
