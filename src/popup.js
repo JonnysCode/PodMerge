a23ea8437e1454408b52c1a5f4bdb5b9f7049436
Jonatha Gruss <jonathangruss@hotmail.com>
2023-11-19 19:00:28 +0100

    Prepare for demo

--- src/popup.js
+++ src/popup.js
@@ -8,17 +8,11 @@ import { sendTabMessage } from './util';
   const syncBtn = document.getElementById('sync-btn');
   const saveBtn = document.getElementById('save-btn');
   const loginBtn = document.getElementById('login-btn');
-  const logBtn = document.getElementById('log-btn');
-  const jsonBtn = document.getElementById('json-btn');
-  const testBtn = document.getElementById('test-btn');
 
   editBtn.addEventListener('click', () => onBtnClick('EDIT'));
   syncBtn.addEventListener('click', () => onBtnClick('SYNC'));
   saveBtn.addEventListener('click', () => onBtnClick('SAVE'));
   loginBtn.addEventListener('click', () => onBtnClick('LOGIN'));
-  logBtn.addEventListener('click', () => onBtnClick('LOG'));
-  jsonBtn.addEventListener('click', () => onBtnClick('JSON'));
-  testBtn.addEventListener('click', () => onBtnClick('TEST'));
 
   function onLoadedDOM() {
     // Restore count value
